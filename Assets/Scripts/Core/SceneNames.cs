cac491c691327f1cdfa299948664e9208936c7ee
S3l3n17y <35997119+S3l3n17y@users.noreply.github.com>
2018-02-24 12:56:54 +0900

    シーン名を提供する静的クラスの追加
    各シーン名をstring型テキストとして提供するSceneNamesの追加

---
+++ Assets/Scripts/Core/SceneNames.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hydrochoerus.Core
+{
+    /// <summary>
+    /// 各シーンをstring型のテキストで提供します。
+    /// </summary>
+    public static class SceneNames
+    {
+        /// <summary>
+        /// タイトルシーンのテキスト
+        /// </summary>
+        public static string TitleScene
+        {
+            get
+            {
+                return "Title";
+            }
+        }
+        /// <summary>
+        /// メニューシーンのテキスト
+        /// </summary>
+        public static string MenuScene
+        {
+            get
+            {
+                return "Menu";
+            }
+        }
+
+        /// <summary>
+        /// ゲームシーンのテキスト
+        /// </summary>
+        public static string GameScene
+        {
+            get
+            {
+                return "Game";
+            }
+        }
+    }
+}
