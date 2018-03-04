7907b13d2535de418f4347906cfd750188673998
S3l3n17y <35997119+S3l3n17y@users.noreply.github.com>
2018-03-04 17:52:36 +0900

    トラッキングカメラ追加
    オブジェクトを追跡するカメラスクリプトの追加

---
+++ Assets/Scripts/Core/TrackingCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingCamera : MonoBehaviour {
+    public GameObject Target;
+    public float Distance;
+    public Vector3 Angle;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
