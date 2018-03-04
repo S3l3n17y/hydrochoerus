60179532bc226b95187afa741fd1097e3ac309d7
S3l3n17y <35997119+S3l3n17y@users.noreply.github.com>
2018-03-04 18:31:18 +0900

    カメラが追従するように変更
    トラッキングカメラが追従するように変更

--- Assets/Scripts/Core/TrackingCamera.cs
+++ Assets/Scripts/Core/TrackingCamera.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrackingCamera : MonoBehaviour {
-    public GameObject Target;
-    public float Distance;
-    public Vector3 Angle;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+namespace Hydrochoerus.Core
+{
+    /// <summary>
+    /// 指定オブジェクトをトラッキングします。
+    /// </summary>
+    public class TrackingCamera : MonoBehaviour {
+
+        public GameObject Target = null;        //トラッキング対象のオブジェクト
+        public float Distance = 1.0f;           //カメラ距離
+        public Vector3 Angle = Vector3.back;    //カメラ角度
+        public Vector3 Offset = Vector3.zero;   //カメラオフセット
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+            if (Target == null) return;//ターゲットが存在しない
+            Vector3 rotation = Target.transform.rotation * Angle;   //ターゲットの向きから角度分ずらす
+            Vector3 position = Offset + Target.transform.position + rotation * Distance;
+            gameObject.transform.position = position;
+            gameObject.transform.LookAt(Target.transform.position + Offset);
+        }
+    }
+
 }
